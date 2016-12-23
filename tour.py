c9ddae088139274f74c3ae227a3fc47ddae23bfe
Aubhro <aubhrosengupta@gmail.com>
2016-12-23 18:58:25 -0500

    Add numbering to squares in order of visitation by Knight

--- tour.py
+++ tour.py
@@ -1,10 +1,22 @@
 from chess_py import *
 
+class Fill_square:
+    color = color.white
+    sym = 0
+
+    def __init__(self):
+        __class__.sym += 1
+        self.symbol = str(__class__.sym)
+
+    def __str__(self):
+        return self.symbol
+
+
 empty = [[None for _ in range(8)] for _ in range(8)]
 my_board = Board(empty)
 
 knight = Knight(color.white, Location(0, 0))
-my_board.place_piece_at_square(knight, Location(0, 0)) #Change starting point here
+my_board.place_piece_at_square(knight, Location(0, 0)) # Change starting point here
 
 moves = knight.possible_moves(my_board)
 while len(moves) > 0:
@@ -25,7 +37,7 @@ while len(moves) > 0:
         my_board.move_piece(move.end_loc, current_loc)
 
     my_board.update(corner_move)
-    my_board.place_piece_at_square(Pawn(color.white, current_loc), current_loc)
+    my_board.place_piece_at_square(Fill_square(), current_loc)
     moves = knight.possible_moves(my_board)
 
 print(my_board)
